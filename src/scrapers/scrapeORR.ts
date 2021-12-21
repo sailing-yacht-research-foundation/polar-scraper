4707a7e9fc020ea82dc7f863c6bbf306388030c2
AlvinJK <ajk.alvin@gmail.com>
2021-12-21 17:58:06 +0700

    Refactoring function

--- src/scrapers/scrapeORR.ts
+++ src/scrapers/scrapeORR.ts
@@ -38,78 +38,7 @@ async function scrapeORR() {
         timeout: defaultORRTimeOut,
         waitUntil: 'load',
       });
-      let certInfo = await page.evaluate(() => {
-        const boatName = document
-          .querySelector('span#boat_name_sail')
-          ?.textContent?.split('   ')[0];
-        const org = 'ORR';
-        const suborg = 'None';
-        const certType = 'ORR Full';
-
-        const builder = document.querySelector('#builder')?.textContent;
-        const owner = document.querySelector('#owner')?.textContent;
-        const certNumber = document.querySelector('#cert_id')?.textContent;
-        const issuedDate = document.querySelector('#date_eff')?.textContent;
-
-        const expireDate = 'Unknown';
-        const measureDate = document.querySelector('#meas_date')?.textContent;
-        const country = 'Unknown'; // maybe they're all usa.
-        const sailNum = document
-          .querySelector('span#boat_name_sail')
-          ?.textContent?.split('   ')[1];
-        const className = document.querySelector('#class')?.textContent;
-        const beamFt = document.querySelector('#beam_max')?.textContent;
-        const draftFt = document.querySelector('#draft_mt')?.textContent;
-        const displacementKg = document.querySelector('#disp_mt')?.textContent;
-        const hasPolars = true;
-        const hasTimeAllowances = true;
-
-        const windSpeeds: number[] = [];
-        let skippedFirst = false;
-        // skip 'Wind Speeds kts' text.
-        document
-          .querySelectorAll<HTMLTableCellElement>(
-            '#polar_speed > div > div > table > thead > tr > th',
-          )
-          .forEach((record) => {
-            if (!skippedFirst) {
-              skippedFirst = true;
-            } else {
-              const wSpeed = record.textContent?.replace('kts', '');
-              if (wSpeed !== undefined) {
-                windSpeeds.push(parseFloat(wSpeed));
-              }
-            }
-          });
-
-        const polars = {
-          windSpeeds,
-          //   beatAngles: optBeatAngles,
-          //   beatVMGs: optBeatSpeedsKts,
-          //   polars: trueWindAngles,
-          //   runVMGs: optRunSpeedsKts,
-          //   gybeAngles: optRunAngles,
-        };
-
-        return {
-          boatName,
-          builder,
-          owner,
-          certNumber,
-          issuedDate,
-          expireDate,
-          measureDate,
-          country,
-          sailNum,
-          className,
-          beamFt,
-          draftFt,
-          displacementKg,
-          hasPolars,
-          hasTimeAllowances,
-          polars,
-        };
-      });
+      let certInfo = await page.evaluate(parsePolarInformations);
       console.log(certInfo);
       break;
     }
@@ -118,6 +47,190 @@ async function scrapeORR() {
   } finally {
     closePageAndBrowser({ page, browser });
   }
+}
+
+function parsePolarInformations() {
+  const boatName = document
+    .querySelector('span#boat_name_sail')
+    ?.textContent?.split('   ')[0];
+  const organization = 'ORR';
+  const subOrganization = 'None';
+  const certType = 'ORR Full';
+
+  const builder = document.querySelector('#builder')?.textContent;
+  const owner = document.querySelector('#owner')?.textContent;
+  const certNumber = document.querySelector('#cert_id')?.textContent;
+  const issuedDate = document.querySelector('#date_eff')?.textContent;
+
+  const expireDate = 'Unknown';
+  const measureDate = document.querySelector('#meas_date')?.textContent;
+  const country = 'Unknown'; // maybe they're all usa.
+  const sailNum = document
+    .querySelector('span#boat_name_sail')
+    ?.textContent?.split('   ')[1];
+  const className = document.querySelector('#class')?.textContent;
+  const beamFt = document.querySelector('#beam_max')?.textContent;
+  const draftFt = document.querySelector('#draft_mt')?.textContent;
+  const displacementKg = document.querySelector('#disp_mt')?.textContent;
+  const hasPolars = true;
+  const hasTimeAllowances = true;
+  const windSpeeds: number[] = [];
+  let skippedFirst = false;
+  // skip 'Wind Speeds kts' text.
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_speed > div > div > table > thead > tr > th',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        const wSpeed = record.textContent?.replace('kts', '');
+        if (wSpeed !== undefined) {
+          windSpeeds.push(parseFloat(wSpeed));
+        }
+      }
+    });
+
+  const optBeatAngles: number[] = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_speed > div > div > table > tbody > tr:nth-child(1) > td',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        const beatAngle = record.textContent?.replace('°', '');
+        if (beatAngle !== undefined) {
+          optBeatAngles.push(parseFloat(beatAngle));
+        }
+      }
+    });
+
+  // opt beat speeds;
+  const optBeatSpeedsKts: number[] = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_speed > div > div > table > tbody > tr:nth-child(2) > td',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        if (record.textContent !== null) {
+          optBeatSpeedsKts.push(parseFloat(record.textContent));
+        }
+      }
+    });
+
+  const trueWindAngles: { twa: number; speeds: number[] }[] = [];
+  let currentIndex = 0;
+
+  document
+    .querySelectorAll(
+      '#polar_speed > div > div > table > tbody > tr > td:nth-child(1)',
+    )
+    .forEach((record) => {
+      if (currentIndex >= 2 && currentIndex <= 11) {
+        const speeds: number[] = [];
+        skippedFirst = false;
+        document
+          .querySelectorAll<HTMLTableCellElement>(
+            `#polar_speed > div > div > table > tbody > tr:nth-child(${
+              currentIndex + 1
+            }) > td`,
+          )
+          .forEach((record) => {
+            if (!skippedFirst) {
+              skippedFirst = true;
+            } else {
+              if (record.textContent !== null) {
+                speeds.push(parseFloat(record.textContent));
+              }
+            }
+          });
+
+        if (record.textContent !== null) {
+          trueWindAngles.push({
+            twa: parseFloat(record.textContent.replace('°', '')),
+            speeds: speeds,
+          });
+        }
+      }
+      currentIndex++;
+    });
+
+  // Optimum run speeds kts : '#polar_speed > div > div > table > tbody > tr:nth-child(13) > td'
+  const optRunSpeedsKts: number[] = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_speed > div > div > table > tbody > tr:nth-child(13) > td',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        if (record.textContent !== null) {
+          optRunSpeedsKts.push(parseFloat(record.textContent));
+        }
+      }
+    });
+
+  // optimum run angles '#polar_speed > div > div > table > tbody > tr:nth-child(14) > td'
+  const optRunAngles: number[] = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_speed > div > div > table > tbody > tr:nth-child(14) > td',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        if (record.textContent !== null) {
+          optRunAngles.push(parseFloat(record.textContent.replace('°', '')));
+        }
+      }
+    });
+
+  const polars = {
+    windSpeeds,
+    beatAngles: optBeatAngles,
+    beatVMGs: optBeatSpeedsKts,
+    polars: trueWindAngles,
+    runVMGs: optRunSpeedsKts,
+    gybeAngles: optRunAngles,
+  };
+
+  return {
+    organization,
+    subOrganization,
+    certType,
+    boatName,
+    builder,
+    owner,
+    certNumber,
+    issuedDate,
+    expireDate,
+    measureDate,
+    country,
+    sailNum,
+    className,
+    beamFt,
+    draftFt,
+    displacementKg,
+    hasPolars,
+    hasTimeAllowances,
+    polars,
+  };
 }
 
 scrapeORR();
