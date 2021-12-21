1b86b83fc0668ccb35cce1c6e2016dd32f846346
AlvinJK <ajk.alvin@gmail.com>
2021-12-21 18:13:39 +0700

    Add time allowance, add loop

--- src/scrapers/scrapeORR.ts
+++ src/scrapers/scrapeORR.ts
@@ -1,20 +1,17 @@
 import puppeteer from 'puppeteer';
-import axios from 'axios';
 
 import { closePageAndBrowser, launchBrowser } from '../utils/puppeteerLauncher';
 import logger from '../logger';
 
 const defaultORRTimeOut = 10000;
 
-async function scrapeORR() {
+async function scrapeORR(year: number) {
   const browser = await launchBrowser();
   let page: puppeteer.Page | undefined;
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-
   //  ORR full certs with polars.
 
-  const url = `https://www.regattaman.com/cert_list.php?yr=${currentYear}&lType=all&org=327&goback=%2Fcertificate_page.php%3Fcp_tab&ctyp=ORR&rnum=0&sort=0&ssort=0&sdir=true&ssdir=true`;
+  const orrFullCerts = [];
+  const url = `https://www.regattaman.com/cert_list.php?yr=${year}&lType=all&org=327&goback=%2Fcertificate_page.php%3Fcp_tab&ctyp=ORR&rnum=0&sort=0&ssort=0&sdir=true&ssdir=true`;
   try {
     page = await browser.newPage();
     await page.goto(url, { timeout: defaultORRTimeOut, waitUntil: 'load' });
@@ -31,22 +28,21 @@ async function scrapeORR() {
       return skuList;
     });
 
-    const orrFullCerts = [];
     for (let i = 0; i < orrCertIds.length; i++) {
       let orrUrl = `https://www.regattaman.com/cert_form.php?sku=${orrCertIds[i]}&rnum=0&sort=undefined&ssort=undefined&sdir=true&ssdir=true`;
       await page.goto(orrUrl, {
         timeout: defaultORRTimeOut,
         waitUntil: 'load',
       });
-      let certInfo = await page.evaluate(parsePolarInformations);
-      console.log(certInfo);
-      break;
+      const certInfo = await page.evaluate(parsePolarInformations);
+      orrFullCerts.push(certInfo);
     }
   } catch (error) {
     logger.error(`Failed to Scrape ORR Certs`, error);
   } finally {
     closePageAndBrowser({ page, browser });
   }
+  return orrFullCerts;
 }
 
 function parsePolarInformations() {
@@ -145,12 +141,12 @@ function parsePolarInformations() {
               currentIndex + 1
             }) > td`,
           )
-          .forEach((record) => {
+          .forEach((innerRecord) => {
             if (!skippedFirst) {
               skippedFirst = true;
             } else {
-              if (record.textContent !== null) {
-                speeds.push(parseFloat(record.textContent));
+              if (innerRecord.textContent !== null) {
+                speeds.push(parseFloat(innerRecord.textContent));
               }
             }
           });
@@ -210,6 +206,125 @@ function parsePolarInformations() {
     gybeAngles: optRunAngles,
   };
 
+  /** Time allowances */
+
+  const windSpeedsTA: number[] = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_time > div > div > table > thead > tr > th',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        const wSpeedTA = record.textContent?.replace('kts', '');
+        if (wSpeedTA !== undefined) {
+          windSpeedsTA.push(parseFloat(wSpeedTA));
+        }
+      }
+    });
+
+  const optBeatAnglesTA = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_time > div > div > table > tbody > tr:nth-child(1) > td',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        const beatAngleTA = record.textContent?.replace('kts', '');
+        if (beatAngleTA !== undefined) {
+          optBeatAnglesTA.push(parseFloat(beatAngleTA));
+        }
+      }
+    });
+
+  // opt beat speeds;
+  const optBeatSpeedsKtsTA: number[] = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_time > div > div > table > tbody > tr:nth-child(2) > td',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        const beatSpeedTA = record.textContent?.replace('kts', '');
+        if (beatSpeedTA !== undefined) {
+          optBeatSpeedsKtsTA.push(parseFloat(beatSpeedTA));
+        }
+      }
+    });
+
+  const trueWindAnglesTA: { twa: number; speeds: number[] }[] = [];
+  currentIndex = 0;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_time > div > div > table > tbody > tr > td:nth-child(1)',
+    )
+    .forEach((record) => {
+      if (currentIndex >= 2 && currentIndex <= 11) {
+        const speeds: number[] = [];
+        skippedFirst = false;
+
+        document
+          .querySelectorAll<HTMLTableCellElement>(
+            `#polar_time > div > div > table > tbody > tr:nth-child(${
+              currentIndex + 1
+            }) > td`,
+          )
+          .forEach((innerRecord) => {
+            if (!skippedFirst) {
+              skippedFirst = true;
+            } else {
+              if (innerRecord.textContent !== null) {
+                speeds.push(parseFloat(innerRecord.textContent));
+              }
+            }
+          });
+
+        if (record.textContent !== null) {
+          trueWindAnglesTA.push({
+            twa: parseFloat(record.textContent.replace('°', '')),
+            speeds: speeds,
+          });
+        }
+      }
+      currentIndex++;
+    });
+
+  const optRunSpeedsKtsTA: number[] = [];
+  skippedFirst = false;
+
+  document
+    .querySelectorAll<HTMLTableCellElement>(
+      '#polar_time > div > div > table > tbody > tr:nth-child(13) > td',
+    )
+    .forEach((record) => {
+      if (!skippedFirst) {
+        skippedFirst = true;
+      } else {
+        if (record.textContent !== null) {
+          optRunSpeedsKtsTA.push(parseFloat(record.textContent));
+        }
+      }
+    });
+
+  const timeAllowances = {
+    windSpeeds: windSpeedsTA,
+    beatVMGs: optBeatSpeedsKtsTA,
+    timeAllowances: trueWindAnglesTA,
+    runVMGs: optRunSpeedsKtsTA,
+  };
+
   return {
     organization,
     subOrganization,
@@ -230,7 +345,17 @@ function parsePolarInformations() {
     hasPolars,
     hasTimeAllowances,
     polars,
+    timeAllowances,
   };
 }
 
-scrapeORR();
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+(async () => {
+  for (let year = 2020; year <= currentYear; year++) {
+    logger.info(`Start scraping ORR year: ${year}`);
+    await scrapeORR(year);
+    logger.info(`Finished scraping ORR year: ${year}`);
+  }
+})();
