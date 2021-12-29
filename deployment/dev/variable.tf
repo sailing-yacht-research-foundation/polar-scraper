3aeee3219abd82ff8d6ae134ba3df470e8a9c68c
Alvin Kurniawan <ajk.alvin@gmail.com>
2021-12-29 13:08:04 +0700

    Terraform Deployment Configuration (#3)
    * Added initial basic terraform configurations
    
    * Working deployment, need update to run within existing cluster
    
    * Successful deploy on scraper runner, need actual test run
    
    * Update logger serviceName
    
    * Set enabled for task to false

---
+++ deployment/dev/variable.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "Which region should the resources be deployed into?"
+}
+
+variable "aws_subnets_cidr" {
+  default     = ["10.16.1.0/24", "10.16.16.0/24", "10.16.32.0/24"]
+  description = "List of Cidr blocks"
+}
+
+variable "aws_availability_zones" {
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  description = "Availability zone list"
+}
+
+variable "iam_ecsTaskExecution_role" {
+  default     = "arn:aws:iam::335855654610:role/ecsTaskExecutionRole"
+  description = "The IAM Role to run ECS Task"
+}
+
+variable "task_name" {
+  default = "polar-scraper-task"
+}
+
+variable "scraper_runner_arn" {
+  default     = "arn:aws:ecs:us-east-1:335855654610:cluster/scraper-runner"
+  description = "The cluster of scraper runner on DEV"
+}
